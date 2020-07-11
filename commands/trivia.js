7c84ad8ed40fa9668134dbaf5065c87ddbd0ef25
Justin McKinney <justinmckinney.md@gmail.com>
2020-07-11 17:44:42 +0000

    add trivia answer shuffling

--- commands/trivia.js
+++ commands/trivia.js
@@ -1,5 +1,5 @@
 const fetch = require('node-fetch');
-
+const shuffle = require('shuffle-array');
 module.exports = {
     name: 'trivia',
     descrption: 'answer a trivia question for gold!',
@@ -7,10 +7,13 @@ module.exports = {
         let response = await fetch('https://opentdb.com/api.php?amount=1');
         let question = await response.json();
         console.log(question);
+        let answers = question.results[0].incorrect_answers.concat(question.results[0].correct_answer);
+        shuffle(answers)
+
         message.channel.send("Category: " + question.results[0].category);
         message.channel.send("Difficulty: " + question.results[0].difficulty);
         message.channel.send("Question = " + question.results[0].question);
-        message.channel.send("Answers = " + question.results[0].incorrect_answers + " " + question.results[0].correct_answer );
+        message.channel.send("Answers = " + answers );
 
     }
 }
