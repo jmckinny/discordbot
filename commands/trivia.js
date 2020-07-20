23a115a935698ca1b94fec1256f7b4ea8bbed56d
Justin McKinney <justinmckinney.md@gmail.com>
2020-07-20 23:44:58 +0000

    add trivia interactivity

--- commands/trivia.js
+++ commands/trivia.js
@@ -4,27 +4,99 @@ module.exports = {
     name: 'trivia',
     descrption: 'answer a trivia question for gold!',
     async execute(message, args, players) {
-        let response = await fetch('https://opentdb.com/api.php?amount=1');
+        let response = await fetch('https://opentdb.com/api.php?amount=1&type=multiple');
         let trivia = await response.json();
         console.log(trivia);
+
         let answers = trivia.results[0].incorrect_answers.concat(trivia.results[0].correct_answer);
         shuffle(answers);
+        let correct;
+        for(let i = 0; i < answers.length; i++){
+            if(answers[i] === trivia.results[0].correct_answer){
+                switch(i){
+                    case 0:
+                        correct = 'a';
+                        break;
+                    case 1:
+                        correct = 'b';
+                        break;
+                    case 2:
+                        correct = 'c';
+                        break;
+                    case 3:
+                        correct = 'd';
+                        break; 
+                }
+                break;
+            }
+        }
+
+        console.log(correct);
+
+
         const category = trivia.results[0].category;
         const difficulty = trivia.results[0].difficulty;
         const question = trivia.results[0].question;
+
+        const A = answers[0];
+        const B = answers[1];
+        const C = answers[2];
+        const D = answers[3];
+
+        
+        
+
+
 
 
 
         const formmated_message = `Question: ${question} 
         Category: ${category}
         Difficulty: ${difficulty}
-        Answers: ${answers}`;
+        A: ${A}
+        B: ${B}
+        C: ${C}
+        D: ${D}`;
 
-        message.channel.send(formmated_message);
-        
 
+        const filter = user_answer =>{
+            switch(user_answer.content.toLowerCase()){
+                case 'a':
+                    return true;
+                case 'b':
+                    return true;
+                case 'c':
+                    return true;
+                case 'd':
+                    return true;
+                default:
+                    return false;
+            }
+        };
 
-        
+        message.channel.send(formmated_message).then(() =>{
+            message.channel.awaitMessages(filter, {max: 1, time: 30000, errors: ['time'] })
+            .then(collected =>{
+                
+                if(collected.first().content.toLowerCase() === correct ){
+                    message.channel.send(`${collected.first().author} responded correctly`);
+                }else{
+                    message.channel.send(`${collected.first().author} responded incorrectly`);
+                }
+                
+            })
+            .catch(collected =>{
+                message.channel.send("Time is up!");
+            })
+            .then(()=>{
+                message.channel.send(`The correct answer was ${correct.toUpperCase()} = ${trivia.results[0].correct_answer}`);
+            })
+            
+        });
+
+       
+
 
     }
 }
+
