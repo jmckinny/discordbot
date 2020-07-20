61a0f179900e2216b05d3603b866d35619b0a472
Justin McKinney <justinmckinney.md@gmail.com>
2020-07-20 06:08:43 +0000

    made trivia message all one message

--- commands/trivia.js
+++ commands/trivia.js
@@ -5,15 +5,26 @@ module.exports = {
     descrption: 'answer a trivia question for gold!',
     async execute(message, args, players) {
         let response = await fetch('https://opentdb.com/api.php?amount=1');
-        let question = await response.json();
-        console.log(question);
-        let answers = question.results[0].incorrect_answers.concat(question.results[0].correct_answer);
-        shuffle(answers)
+        let trivia = await response.json();
+        console.log(trivia);
+        let answers = trivia.results[0].incorrect_answers.concat(trivia.results[0].correct_answer);
+        shuffle(answers);
+        const category = trivia.results[0].category;
+        const difficulty = trivia.results[0].difficulty;
+        const question = trivia.results[0].question;
 
-        message.channel.send("Category: " + question.results[0].category);
-        message.channel.send("Difficulty: " + question.results[0].difficulty);
-        message.channel.send("Question = " + question.results[0].question);
-        message.channel.send("Answers = " + answers );
+
+
+        const formmated_message = `Question: ${question} 
+        Category: ${category}
+        Difficulty: ${difficulty}
+        Answers: ${answers}`;
+
+        message.channel.send(formmated_message);
+        
+
+
+        
 
     }
 }
