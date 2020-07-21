d67868b1178480f6920143fa842111b8c508e2a1
Justin McKinney <justinmckinney.md@gmail.com>
2020-07-21 00:25:38 +0000

    add playing command

---
+++ commands/playing.js
@@ -0,0 +1,13 @@
+module.exports = {
+    name: 'playing',
+    description: 'lists active players',
+    execute(message,args,players){
+        message.channel.send(`There are ${players.size} active players`);
+        let str = ""
+        players.forEach(element => {
+            str += `${element.name} : ${element.gold} gold \n`;
+        });
+        message.channel.send(str);
+    }
+}
+
