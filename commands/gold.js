df20f3ef3ea2611c9c1e8c4af2017bbd880436f3
Justin McKinney <justinmckinney.md@gmail.com>
2020-07-21 05:37:43 +0000

    check if player has joined

--- commands/gold.js
+++ commands/gold.js
@@ -2,17 +2,22 @@ module.exports={
     name: 'gold',
     description: 'returns current gold of the player',
     execute(message,args,players){
-        if(args[0] == null){
-            let player = players.get(message.author.id);
-            message.channel.send(player.mention + " : " + player.gold + " gold");
+        if(!players.has(message.author.id)){
+            message.reply("You have not joined!");
         }else{
-            let member = message.mentions.members.first();
-            if(!players.has(member.id)){
-                message.channel.send("That user does not exist");
-            }else{
-                let player = players.get(member.id);
+            let player = players.get(message.author.id);
+            if(args[0] == null){
                 message.channel.send(player.mention + " : " + player.gold + " gold");
+            }else{
+                let member = message.mentions.members.first();
+                if(!players.has(member.id)){
+                    message.channel.send("That user does not exist");
+                }else{
+                    let player = players.get(member.id);
+                    message.channel.send(player.mention + " : " + player.gold + " gold");
+                }
             }
         }
+        
     }
 }
