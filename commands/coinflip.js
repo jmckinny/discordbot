d2fa59cfcf62f566f70e6c004c87ced5e520415b
Justin McKinney <justinmckinney.md@gmail.com>
2020-07-21 05:14:34 +0000

    add coinflip

--- commands/coinflip.js
+++ commands/coinflip.js
@@ -3,10 +3,27 @@ module.exports={
     descrption: 'a 50% chance to win!',
     execute(message,args,players){
         let player = players.get(message.author.id);
-        if(Math.ceil(Math.random() * 2) === 1){
-            message.channel.send("Winner!");
+        console.log(args[0]);
+        if(args[0] == null){
+            if(Math.ceil(Math.random() * 2) === 1){
+                message.reply("Winner!");
+            }else{
+                message.reply("Loser!");
+            }
         }else{
-            message.channel.send("Loser!");
+            const amount = parseInt(args[0]);
+            if(amount > player.gold){
+                message.reply("You don't have that much gold!");
+                return;
+            }
+            if(Math.ceil(Math.random() * 2) === 1){
+                message.reply(`Winner! ${player.name} wins ${amount} gold.`);
+                player.addGold(amount);
+            }else{
+                message.reply(`Loser! ${player.name} loses ${amount} gold.`);
+                player.addGold(-amount);
+            }
         }
+        
     }
 }
