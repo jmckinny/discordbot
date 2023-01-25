3801193ca98eba347fab54b8c9967e233adc05c5
jmckinny <justinmckinney.md@gmail.com>
2023-01-25 13:08:55 -0500

    inital ping bot

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+use dotenv::dotenv;
+use serenity::async_trait;
+use serenity::model::channel::Message;
+use serenity::model::gateway::Ready;
+use serenity::prelude::*;
+use std::env;
+
+struct Handler;
+#[async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, ctx: Context, msg: Message) {
+        if msg.content == "!ping" {
+            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
+                println!("ERROR: failed to send message due to {:?}", why);
+            }
+        }
+    }
+
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected", ready.user.name);
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+
+    let token = env::var("DISCORD_TOKEN").expect("ERROR: Failed to load token");
+    let intents = GatewayIntents::GUILD_MESSAGES
+        | GatewayIntents::MESSAGE_CONTENT
+        | GatewayIntents::DIRECT_MESSAGES;
+
+    let mut client = Client::builder(&token, intents)
+        .event_handler(Handler)
+        .await
+        .expect("ERROR: failed to create client");
+    if let Err(why) = client.start().await {
+        println!("ERROR: start failed due to {:?}", why);
+    }
+}
