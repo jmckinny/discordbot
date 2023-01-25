3dd90b7607c90007239ab54bbcb0ff55e9269fdd
jmckinny <justinmckinney.md@gmail.com>
2023-01-25 14:00:37 -0500

    more advanced bot structure using framework

---
+++ src/commands/info.rs
@@ -0,0 +1,15 @@
+use serenity::framework::standard::macros::command;
+use serenity::framework::standard::CommandResult;
+use serenity::model::prelude::*;
+use serenity::prelude::*;
+
+#[command]
+pub async fn info(ctx: &Context, msg: &Message) -> CommandResult {
+    const LINK: &str = "https://github.com/jmckinny/frothybot";
+    let mssg = format!("I am being rewritten in rust using the serenity framework!\n You can monitor my progress here <{}>",LINK);
+    msg.author
+        .direct_message(&ctx.http, |m| m.content(&mssg))
+        .await?;
+    msg.react(&ctx, '👌').await?;
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,121 @@
+mod commands;
+use crate::commands::info::*;
+use crate::commands::ping::*;
+
 use dotenv::dotenv;
+use serenity::framework::standard::CommandResult;
+use std::collections::HashSet;
+use std::env;
+use std::sync::Arc;
+
 use serenity::async_trait;
+use serenity::client::bridge::gateway::ShardManager;
+use serenity::framework::standard::macros::group;
+use serenity::framework::standard::macros::hook;
+use serenity::framework::StandardFramework;
+use serenity::http::Http;
 use serenity::model::channel::Message;
+use serenity::model::event::ResumedEvent;
 use serenity::model::gateway::Ready;
 use serenity::prelude::*;
-use std::env;
+use tracing::{error, info};
+
+pub struct ShardManagerContainer;
+
+impl TypeMapKey for ShardManagerContainer {
+    type Value = Arc<Mutex<ShardManager>>;
+}
 
 struct Handler;
 #[async_trait]
 impl EventHandler for Handler {
-    async fn message(&self, ctx: Context, msg: Message) {
-        if msg.content == "!ping" {
-            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
-                println!("ERROR: failed to send message due to {:?}", why);
-            }
-        }
+    async fn ready(&self, _: Context, ready: Ready) {
+        info!("Connected as {}", ready.user.name);
     }
 
-    async fn ready(&self, _: Context, ready: Ready) {
-        println!("{} is connected", ready.user.name);
+    async fn resume(&self, _: Context, _: ResumedEvent) {
+        info!("Resumed");
     }
+}
+
+#[group]
+#[commands(ping, info)]
+struct General;
+
+//Hooks
+#[hook]
+async fn after(_ctx: &Context, _msg: &Message, command_name: &str, command_result: CommandResult) {
+    match command_result {
+        Ok(()) => info!("Processed command '{}'", command_name),
+        Err(why) => error!("Command '{}' returned error {:?}", command_name, why),
+    }
+}
+
+#[hook]
+async fn before(_ctx: &Context, msg: &Message, command_name: &str) -> bool {
+    info!(
+        "Got command '{}' by user '{}'",
+        command_name, msg.author.name
+    );
+    true // if `before` returns false, command processing doesn't happen.
+}
+
+#[hook]
+async fn unknown_command(_ctx: &Context, _msg: &Message, unknown_command_name: &str) {
+    info!("Could not find command named '{}'", unknown_command_name);
 }
 
 #[tokio::main]
 async fn main() {
     dotenv().ok();
+    tracing_subscriber::fmt::init();
 
     let token = env::var("DISCORD_TOKEN").expect("ERROR: Failed to load token");
+
+    let http = Http::new(&token);
+
+    let (owners, _bot_id) = match http.get_current_application_info().await {
+        Ok(info) => {
+            let mut owners = HashSet::new();
+            owners.insert(info.owner.id);
+
+            (owners, info.id)
+        }
+        Err(why) => panic!("Could not access application info: {:?}", why),
+    };
+
+    let framework = StandardFramework::new()
+        .configure(|c| c.owners(owners).prefix("!"))
+        .after(after)
+        .before(before)
+        .unrecognised_command(unknown_command)
+        .group(&GENERAL_GROUP);
+
     let intents = GatewayIntents::GUILD_MESSAGES
         | GatewayIntents::MESSAGE_CONTENT
         | GatewayIntents::DIRECT_MESSAGES;
 
     let mut client = Client::builder(&token, intents)
+        .framework(framework)
         .event_handler(Handler)
         .await
         .expect("ERROR: failed to create client");
+
+    {
+        let mut data = client.data.write().await;
+        data.insert::<ShardManagerContainer>(client.shard_manager.clone());
+    }
+
+    let shard_manager = client.shard_manager.clone();
+
+    tokio::spawn(async move {
+        tokio::signal::ctrl_c()
+            .await
+            .expect("Could not register ctrl+c handler");
+        shard_manager.lock().await.shutdown_all().await;
+    });
+
     if let Err(why) = client.start().await {
-        println!("ERROR: start failed due to {:?}", why);
+        error!("ERROR: start failed due to {:?}", why);
     }
 }
