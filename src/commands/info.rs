c1a890e16b574d85554e84700a25bad0eaa7dc4f
jmckinny <justinmckinney.md@gmail.com>
2023-01-27 12:37:17 -0500

    fix clippy warning

--- src/commands/info.rs
+++ src/commands/info.rs
@@ -6,7 +6,7 @@ use serenity::prelude::*;
 #[command]
 pub async fn info(ctx: &Context, msg: &Message) -> CommandResult {
     const LINK: &str = "https://github.com/jmckinny/frothybot";
-    let mssg = format!("I am being rewritten in rust using the serenity framework!\n You can monitor my progress here <{}>",LINK);
+    let mssg = format!("I am being rewritten in rust using the serenity framework!\n You can monitor my progress here <{LINK}>",);
     msg.author
         .direct_message(&ctx.http, |m| m.content(&mssg))
         .await?;
