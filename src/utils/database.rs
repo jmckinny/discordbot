8611c4b6f4f855510fd9cc5652f0db658ad6ddf2
Justin McKinney <justinmckinney.md@gmail.com>
2025-03-16 14:16:37 -0400

    fix: update to token

--- src/utils/database.rs
+++ src/utils/database.rs
@@ -6,7 +6,9 @@ use std::fs::File;
 use std::io::{Error, Write};
 use tracing::info;
 
-pub fn save_data(data: HashMap<UserId, u64>) -> Result<(), Error> {
+use crate::Token;
+
+pub fn save_data(data: HashMap<UserId, Token>) -> Result<(), Error> {
     let database = Database { data };
     let json = serde_json::to_string(&database)?;
     let mut f = File::create("database.json")?;
@@ -16,7 +18,7 @@ pub fn save_data(data: HashMap<UserId, u64>) -> Result<(), Error> {
     Ok(())
 }
 
-pub fn load_data() -> Result<HashMap<UserId, u64>, Error> {
+pub fn load_data() -> Result<HashMap<UserId, Token>, Error> {
     let json = std::fs::read_to_string("database.json")?;
     let database: Database = serde_json::from_str(&json)?;
     info!("Loaded {} entries successfully", database.data.len());
